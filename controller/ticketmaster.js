225679e6d86f0d413addd5af29b4d4e0ec8831b1
bellafkb b <bellafkb@gmail.com>
2019-08-04 17:18:19 +0200

    bug fix

--- controller/ticketmaster.js
+++ controller/ticketmaster.js
@@ -5,12 +5,10 @@ const ticketmaster = {};
 
 ticketmaster.fetchEventsByLocation = (req, res) => {
     axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${config.TICKETMASTER_KEY}`)
-        .then( (content) => {
-            console.log(content)
-            res.send(content)
+        .then( ({data}) => {
+            res.send(data)
 
         }).catch( (error) => {
-            console.log(error)
             res.json({
                 error
             })
